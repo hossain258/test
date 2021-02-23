5201ee4b0af35712645f4424a31100ecad3007de
hossain <hossain258@gmail.com>
2021-02-23 14:14:19 +0000

    test

---
+++ complex.py
@@ -0,0 +1,6 @@
+def pyramid(n):
+    for i in range(n):
+        print(' '*(n-i-1) + '* '*(i+1) )
+    for i in range(n):
+        print(' '*(i+1) + '* '*(n-i-1))
+pyramid(8)
---
+++ test.py
@@ -0,0 +1,11 @@
+lenth = input("What is your name ?    ")
+birth_year = input("what is your birth year ? ")
+age = 2021-int(birth_year)
+print("\n\n")
+print("My details\n")
+print("................\n")
+
+print("my name is :" , lenth)
+print("My age is :" , age)
+
+
